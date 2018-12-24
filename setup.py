64504ffe908694b1e9d417062642aa50aef7eb65
Brandon <brandon-spendlove@hotmail.co.uk>
2018-12-24 22:48:43 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='BCPTools',
+	version='0.1',
+	description='Brandons Cisco Project Tools',
+	url='https://github.com/BSpendlove/BCP-Tools',
+	author='BSpendlove',
+	author_email="brandon.spendlove@w17bs.co.uk",
+	packages=['BCPTools'],
+	include_package_data=True
+)
